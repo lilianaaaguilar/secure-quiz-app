bb16b968de663b63fb031653df34795ff2f54af4
Liliana Aguilar <lilianaguilar2179@icloud.com>
2025-11-18 13:42:40 -0800

    Feat: finish all python needed

--- webapp.py
+++ webapp.py
@@ -1,4 +1,5 @@
 import os
+import time
 from flask import Flask, url_for, render_template, request
 from flask import redirect
 from flask import session
@@ -24,33 +25,71 @@ def startOver():
 
 @app.route('/page1')
 def renderPage1():
-    return render_template('page1.html')
+	session["startTime"] = time.time()
+	return render_template('page1.html')
 
 @app.route('/page2',methods=['GET','POST'])
 def renderPage2():
-    session["firstResponse"]=request.form['firstResponse']
-    return render_template('page2.html')
+	if "firstResponse" not in session:
+		session["firstResponse"]=request.form['firstResponse']
+	return render_template('page2.html')
 
 @app.route('/page3',methods=['GET','POST'])
 def renderPage3():
-    session["secondResponse"]=request.form['secondResponse']
-    return render_template('page3.html')
+	if "secondResponse" not in session:
+		session["secondResponse"]=request.form['secondResponse']
+	return render_template('page3.html')
 
 @app.route('/page4',methods=['GET','POST'])
 def renderPage4():
-    session["thirdResponse"]=request.form['thirdResponse']
-    return render_template('page4.html')
+	if "thirdResponse" not in session:
+		session["thirdResponse"]=request.form['thirdResponse']
+	return render_template('page4.html')
     
 @app.route('/page5',methods=['GET','POST'])
 def renderPage5():
-    session["fourthResponse"]=request.form['fourthResponse']
-    return render_template('page5.html')
+	if "fourthResponse" not in session:
+		session["fourthResponse"]=request.form['fourthResponse']
+	return render_template('page5.html')
     
 @app.route('/page6',methods=['GET','POST'])
 def renderPage6():
-    session["fifthResponse"]=request.form['fifthResponse']
-    return render_template('page6.html')
+    if "fifthResponse" not in session:
+        session["fifthResponse"]=request.form['fifthResponse']
+    session["endTime"] = time.time()
+    score = 0
+    if session["firstResponse"].lower() == "egg":
+	    answer1="Correct"
+	    score=score+1
+    else:
+	    answer1="Incorrect"
+        
+    if session["secondResponse"] == "12" or session["secondResponse"].lower() == "twelve":
+        answer2="Correct"
+        score=score+1
+    else:
+    	answer2="Incorrect"
     
+    if session["thirdResponse"].lower() == "age":
+        answer3="Correct"
+        score=score+1
+    else:
+        answer3="Incorrect"
+        
+    if session["fourthResponse"].lower() == "darkness":
+        answer4="Correct"
+        score=score+1
+    else:
+        answer4="Incorrect"
+        
+    if session["fifthResponse"].lower() == "cold":
+        answer5="Correct"
+        score=score+1
+    else:
+       answer5="Incorrect"
+         
+    timeTaken = session["endTime"] - session["startTime"]
+    return render_template('page6.html', answer1=answer1, answer2=answer2, answer3=answer3, answer4=answer4, answer5=answer5, score=score, timeTaken=timeTaken)
     
 if __name__=="__main__":
     app.run(debug=False)
